cbadcfa9737f57d0fbe53461c0f38dc3f01ec8da
haribasker1997 <31512665+haribasker1997@users.noreply.github.com>
2017-09-02 20:59:21 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class array
+{
+public static void main(String args[])
+{
+int sum=0,i;
+for(i=1;i<=10;i++)
+{
+sum=sum+i;
+}
+System.out.println("sum is",+sum);
+}
+}
